944a757f2800b66f752294860387b93bce74d8b8
Prashant Sinha <psinha@predictiveindex.com>
2022-04-08 20:13:10 -0400

    Bug fix - Initialized columns list

--- BuildIntelligence.Domo.Sdk/Datasets/Schema.cs
+++ BuildIntelligence.Domo.Sdk/Datasets/Schema.cs
@@ -7,6 +7,7 @@ namespace BuildIntelligence.Domo.Sdk.Datasets
 	public class Schema
 	{
         public Schema() { }
+		public List<Column> Columns { get; set; }
 
         /// <summary>
         /// Construct Schema from Dictionary of column names and types
@@ -14,12 +15,12 @@ namespace BuildIntelligence.Domo.Sdk.Datasets
         /// <param name="columns">string key for column name, and column type for value</param>
         public Schema(IDictionary<string,DomoDataType> columns)
         {
+            Columns = new List<Column>();
             foreach(var col in columns)
             {
                 Columns.Add(new Column(col.Key, col.Value));
             }
         }
-		public List<Column> Columns { get; set; }
 	}
 
 	public class Column
